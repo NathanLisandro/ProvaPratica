22cbe1d4703cb80b1531711ae8e23671a467756b
Nathan Lisandro Toppa <78770036+NathanLisandro@users.noreply.github.com>
2024-04-24 19:30:21 -0300

    Adicionando coisas no script.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function consultaApiIBGE () {
     const response = fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados');
     console.log(response)
+    const dados = response.data
+    let ul = document.getElementById('listaDeEstados');
+    for(let i = 0; i < dados.length; i++){
+        let li = document.createElement('li');
+        li.innerText = dados[i].nome;
+        ul.appendChild(li)
+    }
 }
 
 consultaApiIBGE()
