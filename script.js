f86ef2679393aa6aeceb1011826971fcdf8af0f7
Nathan Lisandro Toppa <78770036+NathanLisandro@users.noreply.github.com>
2024-04-24 19:36:40 -0300

    dados

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 async function consultaApiIBGE () {
     const response = await fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados');
-    console.log(response)
-    const dados = response.data
+    const dados = await response.json();
+console.log(dados)
     let ul = document.getElementById('listaDeEstados');
     for(let i = 0; i < dados.length; i++){
         let li = document.createElement('li');
